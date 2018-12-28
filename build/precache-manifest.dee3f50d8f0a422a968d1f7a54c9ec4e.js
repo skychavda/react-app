a3a0e880b5d5a0b86979a5bc3e7597d5dedd0c66
Akash Chavda <skychavda9@gmail.com>
2018-12-28 18:09:17 +0530

    commit

---
+++ build/precache-manifest.dee3f50d8f0a422a968d1f7a54c9ec4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba490551b7fa698c1b68",
+    "url": "/static/js/main.ba490551.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ba490551b7fa698c1b68",
+    "url": "/static/css/main.580278f5.chunk.css"
+  },
+  {
+    "revision": "b1af497c47c6229ef690ab2ae9263759",
+    "url": "/index.html"
+  }
+];
